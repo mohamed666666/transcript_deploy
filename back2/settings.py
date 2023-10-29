edb2ff1effc7e1dbed0ff876e0e30dbf3f023b61
mohamed666666 <mohamedaswafi@gmail.com>
2023-10-29 20:20:21 +0200

    first commit121101

--- back2/settings.py
+++ back2/settings.py
@@ -54,6 +54,7 @@ CHANNEL_LAYERS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'channels.middleware.BaseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
